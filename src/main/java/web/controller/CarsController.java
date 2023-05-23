d88db2cd6c2222090d11fd590823238bc5706ff7
Daniil Rubak <arhdark@yandex.ru>
2023-05-23 13:31:34 +0300

    add @RequestParam ref cars.html

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,13 +4,11 @@ import model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import servise.CarsList;
 import servise.CarsListImp;
-
 import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
 public class CarsController {
