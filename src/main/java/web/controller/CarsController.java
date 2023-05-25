7b77b434b7c718b576205d90b58b15a9186d6d3b
Daniil Rubak <arhdark@yandex.ru>
2023-05-25 14:10:21 +0300

    add @Been & Config. ref CarsController

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,21 +1,13 @@
 package web.controller;
 
 import config.AppConfig;
-import model.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import servise.CarService;
-import servise.CarServiceImpl;
-import java.util.ArrayList;
-
-import static java.lang.Double.TYPE;
-import static java.lang.Double.isNaN;
-
 
 @Controller
 public class CarsController {
